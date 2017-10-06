4277834cf4287f8786e53513376cfb603a66d71e
Rigel-Sile <jsilva@uni.pe>
2017-10-06 18:26:17 -0500

    persistent menu

--- app.js
+++ app.js
@@ -69,7 +69,51 @@ app.get('/setup',function(req,res){
     setupGetStartedButton(res);
 });
 
-
+// persistent button
+app.get('/persistent-menu',function(req, res){
+  var menu = {
+  "persistent_menu":[
+    {
+      "locale":"default",
+      "composer_input_disabled":true,
+      "call_to_actions":[
+        {
+          "title":"My Account",
+          "type":"nested",
+          "call_to_actions":[
+            {
+              "title":"Pay Bill",
+              "type":"postback",
+              "payload":"PAYBILL_PAYLOAD"
+            },
+            {
+              "title":"History",
+              "type":"postback",
+              "payload":"HISTORY_PAYLOAD"
+            },
+            {
+              "title":"Contact Info",
+              "type":"postback",
+              "payload":"CONTACT_INFO_PAYLOAD"
+            }
+          ]
+        },
+        {
+          "type":"web_url",
+          "title":"Latest News",
+          "url":"http://petershats.parseapp.com/hat-news",
+          "webview_height_ratio":"full"
+        }
+      ]
+    },
+    {
+      "locale":"zh_CN",
+      "composer_input_disabled":false
+    }
+  ]
+};
+  callSendAPI(menu);
+});
 
 // Display the web page
 app.get('/', function(req, res) {
@@ -274,4 +318,4 @@ function setupGetStartedButton(res){
                 res.send(body);
             }
         });
-    }
+}
